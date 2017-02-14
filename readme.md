e83e804c0b01be10d1db6f9443b2104768a404b7
Doug Duril <doug.duril@gmail.com>
2017-02-14 10:42:56 -0800

    Add R examples for Learning R Chapter 1

--- readme.md
+++ readme.md
@@ -5,8 +5,13 @@
 - **<a href="https://www.r-project.org/">R Project for Statistical Computing</a>**
 - **<a href="https://www.rstudio.com/">RStudio</a>**
 
-
 ### Packages
 
 - **<a href="http://rmarkdown.rstudio.com/">R Markdown</a>**
 - **<a href="http://tidyverse.org/">tidyverse</a>**
+
+### Books
+
+- **Learning R** (O'Reilly)
+- **R Cookbook** (O'Reilly)
+- **R for Data Science** (O'Reilly)
---
+++ scripts/learning_r_chapter_1.R
@@ -0,0 +1,41 @@
+# R Programming
+# Learning R
+# Chapter 1
+
+# first R program
+mean(1:5)
+
+# getting help
+help("mean")
+help("if")
+help.search("plotting")
+
+# can also use a single or double question marks
+?mean
+?"if"
+??plotting
+??"regression model"
+
+# vector example
+a_vector <- c(1, 3, 6, 10, 14)
+a_vector
+
+# a few more vector examples
+x_vector <- c(0,1,1,2,3,5,8,13,21,34)
+x_vector
+mean(x_vector)
+sd(x_vector)
+median(x_vector)
+
+# vector with NA values and how to handle
+y_vector <- c(0,1,1,2,NA,3,5,8,13,21,34,NA)
+y_vector
+mean(y_vector, na.rm=TRUE)
+sd(y_vector, na.rm=TRUE)
+median(y_vector, na.rm=TRUE)
+
+# variables that contain the string 'vector'
+apropos("vector")
+
+example(plot)
+demo(barplot())

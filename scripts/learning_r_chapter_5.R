e49b48ec4b64be64bf617c9d9e16eb3974aa9a94
Doug Duril <doug.duril@gmail.com>
2017-02-14 11:24:50 -0800

    Add Learning R Ch5 list example

---
+++ scripts/learning_r_chapter_5.R
@@ -0,0 +1,22 @@
+# R Programming
+# Learning R
+# Chapter 5
+
+# lists
+a_list <- c(12, 23, 15, 18, 25)
+a_list
+
+# add names
+names(a_list) <- c("Apples", "Bananas", "Cherries", "Oranges", "Pears")
+a_list
+
+
+length(a_list)
+dim(a_list)
+nrow(a_list)
+ncol(a_list)
+NROW(a_list)
+NCOL(a_list)
+
+a_list[[1]]
+a_list[["Cherries"]]

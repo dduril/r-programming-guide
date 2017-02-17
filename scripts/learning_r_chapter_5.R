0b7e5e9c873fa149558a777b4084018c0d09c8e7
Doug Duril <doug.duril@gmail.com>
2017-02-17 08:56:04 -0800

    Add data frame example

--- scripts/learning_r_chapter_5.R
+++ scripts/learning_r_chapter_5.R
@@ -24,3 +24,35 @@ NCOL(a_list)
 
 a_list[[1]]
 a_list[["Cherries"]]
+
+# data frame
+(a_data_frame <- data.frame(
+  x = letters[1:5],
+  y = c(1:5),
+  z = runif(5) > 0.5,
+  row.names = c("foo", "bar", "baz", "qud", "quud")
+))
+
+# data frame functions
+class(a_data_frame)
+
+rownames(a_data_frame)
+colnames(a_data_frame)
+dimnames(a_data_frame)
+
+nrow(a_data_frame)
+ncol(a_data_frame)
+dim(a_data_frame)
+
+length(a_data_frame)
+names(a_data_frame)
+
+
+
+
+
+
+
+
+
+
